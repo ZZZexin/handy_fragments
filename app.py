b78c0bc4fa8b2ec25314d0bdee48415d6370b22a
zzzexin <zexinyu@wirelineservices.com.au>
2026-04-24 22:37:16 +0800

    streamlit construction v0.1

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+                    page_title="Handy Tools", layout = "wide",
+                   initial_sidebar_state="expanded" 
+                   )
+
+st.title("Handy Tools")
+
+st.markdown(
+    "Under construction."
+)
