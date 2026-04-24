cdd7648fd13e3d625aa0b55696173037cf2ed74f
zzzexin <zexinyu@wirelineservices.com.au>
2026-04-24 22:06:20 +0800

    finish building read_well_report

--- src/utils/read_well_report.py
+++ src/utils/read_well_report.py
@@ -2,6 +2,7 @@
 
 from openpyxl import load_workbook
 from pathlib import Path
+import pandas as pd
 import json
 
 
@@ -9,7 +10,10 @@ class ReadWellReport:
     def __init__(self):
         self.json_load = False
         self.wellreport = None
+
     def header_info(self, wb_path):
+        if not self.json_load:
+            raise ValueError("Fetch well report location first")
         field_details = self.wellreport["field_details"]
         wb = load_workbook(wb_path, data_only=True)
         ws = wb.active
@@ -17,16 +21,42 @@ class ReadWellReport:
         for field_name, cell_ref in field_details.items():
             result[field_name] = ws[cell_ref].value
         return result
-    def area_info(self, wb_path, area_searched = "logging")
+    
+    def area_info(self, wb_path, area_searched = "logging"):
+        if not self.json_load:
+            raise ValueError("Fetch well report location first")
         if area_searched != "logging" &  area_searched != "cal":
             raise ValueError("Input correct table area, logging or cal") 
-        
+        key = f'{area_searched}_details'
+        details = self.wellreport[key]
 
+        wb = load_workbook(wb_path, data_only=True)
+        ws = wb.active
+        start = details["_table_start_row"]
+        end = details["_table_end_row"]
+
+        data = {}
+
+        for k, v in details.items():
+            if k.startwith("_"):
+                continue
+            values = []
+            for row in range(start, end+1):
+                cell = f"{v}{row}"
+                values.append(ws[cell].value)
+            
+            data[k] = values
+
+        df = pd.DataFrame(data)
+        return df # get the final extraction table
+ 
     def _load_json(self):
         self.json_load = True
         project_root = Path(__file__).resolve().parent
         json_path = project_root / "src" / "config" / "well_report_src.json"
         with open(json_path, "r", encoding="utf-8") as f:
-            self.wellreport = json.load(f)[area]
+            self.wellreport = json.load(f)
             f.close()
+
+
 
